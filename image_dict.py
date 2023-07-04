faf49e2b546ec79db434d466ff5a5f6c8675af06
EC2 Default User <ec2-user@ip-172-31-7-32.us-east-2.compute.internal>
2023-07-04 20:47:00 +0000

    Refactor endpoints

--- image_dict.py
+++ image_dict.py
@@ -1,16 +1,19 @@
 import os
 
-# path = "/Users/sneha/dog-classifier/Images"
+path = "data/"
 
 # Create an empty dictionary to store the dog names
-dog_names = {0: 'silky terrier', 1: 'Scottish deerhound', 2: 'Chesapeake Bay retriever', 3: 'Ibizan hound', 4: 'wire', 5: 'Saluki', 6: 'cocker spaniel', 7: 'schipperke', 8: 'borzoi', 9: 'Pembroke', 10: 'komondor', 11: 'Staffordshire bullterrier', 12: 'standard poodle', 13: 'Eskimo dog', 14: 'English foxhound', 15: 'golden retriever', 16: 'Sealyham terrier', 17: 'Japanese spaniel', 18: 'miniature schnauzer', 19: 'malamute', 20: 'malinois', 21: 'Pekinese', 22: 'giant schnauzer', 23: 'Mexican hairless', 24: 'Doberman', 25: 'standard schnauzer', 26: 'dhole', 27: 'German shepherd', 28: 'Bouvier des Flandres', 29: 'Siberian husky', 30: 'Norwich terrier', 31: 'Irish terrier', 32: 'Norfolk terrier', 33: 'Saint Bernard', 34: 'Border terrier', 35: 'briard', 36: 'Tibetan mastiff', 37: 'bull mastiff', 38: 'Maltese dog', 39: 'Kerry blue terrier', 40: 'kuvasz', 41: 'Greater Swiss Mountain dog', 42: 'Lakeland terrier', 43: 'Blenheim spaniel', 44: 'basset', 45: 'West Highland white terrier', 46: 'Chihuahua', 47: 'Border collie', 48: 'redbone', 49: 'Irish wolfhound', 50: 'bluetick', 51: 'miniature poodle', 52: 'Cardigan', 53: 'EntleBucher', 54: 'Norwegian elkhound', 55: 'German short', 56: 'Bernese mountain dog', 57: 'papillon', 58: 'Tibetan terrier', 59: 'Gordon setter', 60: 'American Staffordshire terrier', 61: 'vizsla', 62: 'kelpie', 63: 'Weimaraner', 64: 'miniature pinscher', 65: 'boxer', 66: 'chow', 67: 'Old English sheepdog', 68: 'pug', 69: 'Rhodesian ridgeback', 70: 'Scotch terrier', 71: 'Shih', 72: 'affenpinscher', 73: 'whippet', 74: 'Sussex spaniel', 75: 'otterhound', 76: 'flat', 77: 'English setter', 78: 'Italian greyhound', 79: 'Labrador retriever', 80: 'collie', 81: 'cairn', 82: 'Rottweiler', 83: 'Australian terrier', 84: 'toy terrier', 85: 'Shetland sheepdog', 86: 'African hunting dog', 87: 'Newfoundland', 88: 'Walker hound', 89: 'Lhasa', 90: 'beagle', 91: 'Samoyed', 92: 'Great Dane', 93: 'Airedale', 94: 'bloodhound', 95: 'Irish setter', 96: 'keeshond', 97: 'Dandie Dinmont', 98: 'basenji', 99: 'Bedlington terrier', 100: 'Appenzeller', 101: 'clumber', 102: 'toy poodle', 103: 'Great Pyrenees', 104: 'English springer', 105: 'Afghan hound', 106: 'Brittany spaniel', 107: 'Welsh springer spaniel', 108: 'Boston bull', 109: 'dingo', 110: 'soft', 111: 'curly', 112: 'French bulldog', 113: 'Irish water spaniel', 114: 'Pomeranian', 115: 'Brabancon griffon', 116: 'Yorkshire terrier', 117: 'groenendael', 118: 'Leonberg', 119: 'black'}
+dog_names = {}
 
-# # Iterate over the subdirectories in the path
-# i=0
-# for subdir in os.listdir(path):
-#     if subdir[0] == 'n':
-#         # Extract the dog breed name from the directory name
-#         breed_name = subdir.split("-")[1].replace("_", " ")
-#         # Add the dog breed name to the dictionary with the corresponding directory name as the value
-#         dog_names[i] = breed_name
-#         i+=1
+# Iterate over the subdirectories in the path
+def populate_dog_names():
+    i=0
+    for subdir in os.listdir(path):
+        if subdir[0] == 'n':
+            # Extract the dog breed name from the directory name
+            breed_name = subdir.split("-")[1].replace("_", " ")
+            # Add the dog breed name to the dictionary with the corresponding directory name as the value
+            dog_names[i] = breed_name
+            i+=1
+
+# populate_dog_names()
--- server.py
+++ server.py
@@ -1,43 +1,95 @@
+# Import libraries
+import os
+import glob
 import numpy as np
-from flask import Flask, render_template, request, redirect
+
+import tensorflow as tf
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
-from image_dict import dog_names 
+
+from flask import Flask, render_template, request
+import requests as rq
+from federated_functions import *
+from training import *
 
 app = Flask(__name__)
+global_model = load_model('model_20.h5')
 
-model = load_model('model_120.h5')
-def predict_label(img_path):
-	img = image.load_img(img_path, target_size=(200, 200))
-	img = image.img_to_array(img)
-	img = img.reshape(1, 200, 200, 3)
-	p = model.predict(img)
-	print(p)
-	return dog_names[np.argmax(p)]
+dic = {0: 'Maltese_dog', 1: 'Rottweiler', 2: 'Pekinese', 3: 'Blenheim_spaniel', 4: 'Basset', 5: 'Japanese_spaniel', 6: 'Chihuahua', 7: 'Afghan_hound', 8: 'Beagle', 9: 'Doberman', 10: 'Papillon', 11: 'Shih_Tzu', 12: 'Rhodesian_ridgeback', 13: 'Golden_retriever', 14: 'Siberian_husky', 15: 'Toy_terrier', 16: 'Labrador_retriever', 17: 'German_shepherd', 18: 'Bloodhound', 19: 'Bluetick'}
 
-# Routes
-@app.route("/", methods=['GET'])
-def homepage():
-	return render_template("home.html")
+def make_prediction(X):
+    pred = global_model.predict(X)
+    return dic[np.argmax(pred)]
 
-@app.route("/about", methods=['GET'])
-def about_page():
-	#return "Sneha Seenuvasavarathan - https://www.linkedin.com/in/svsneha/"
-	return redirect("https://www.linkedin.com/in/svsneha/", code=302)
+def update_model(new_X, new_y):
 
-@app.route("/submit", methods = ['POST'])
-def send_img():
-	img = request.files['my_image']
-	img_path = "static/uploads/" + img.filename
-	img.save(img_path)
-	p = predict_label(img_path)
-	return render_template("prediction.html", prediction = p, img_path = img_path)
+    global_weights = global_model.get_weights()
+    local_model = create_model()
+    local_model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
+    local_model.summary()
+    local_model.set_weights(global_weights)
+    local_model.fit(new_X, new_y, epochs=8)
 
-@app.route("/question", methods = ['GET','POST'])
-def send_answer():
-	# qn = request.json['question']
-	# print(qn)
-	return 'yagaa'
+    scaling_factor = weight_scaling_factor(3614, 3614) # 30/3000 = 1/100 = 0.01
+    local_weights = local_model.get_weights()
+    a = scale_model_weights(global_weights, 1-scaling_factor)
+    b = scale_model_weights(local_weights, scaling_factor)
+    avg_weights = sum_scaled_weights([a,b])
+    global_model.set_weights(avg_weights)
+
+    global_model.save('model_20-1.h5')
+
+@app.route("/predict", methods=['POST', 'GET'])
+def predict():
+    if request.method == 'GET':
+        return 'Hola'
+    img = request.files['image']
+    img_path = "static/uploads/" + img.filename
+    img.save(img_path)
+    img = image.load_img(img_path, target_size=(200, 200))
+    img = image.img_to_array(img)
+    X = img.reshape(1, 200, 200, 3)
+    return make_prediction(X)
+
+@app.route("/update", methods=['POST'])
+def update():
+    no_of_files = int(request.form.get('no_of_files'))
+    image_paths = []
+    for i in range(no_of_files):
+        img = request.files['file'+str(i+1)]
+        img_path = "static/uploads/" + str(i) + img.filename
+        img.save(img_path)
+        image_paths.append(img_path)
+    new_X = np.zeros((no_of_files, 200, 200, 3))
+    for i,path in enumerate(image_paths):
+        img = image.load_img(path, target_size=(200, 200))
+        img = image.img_to_array(img)
+        new_X[i] = img
+
+    label = request.form.get('label')
+    new_y = convert_label(label, no_of_files)
+    update_model(new_X, new_y)
+    return "Success"
+
+@app.route("/new_class", methods=['POST'])
+def new_class():
+    label = request.form.get('label')
+    os.mkdir('data/'+label)
+    no_of_files = int(request.form.get('no_of_files'))
+    image_paths = []
+    for i in range(no_of_files):
+        img = request.files['file'+str(i+1)]
+        img_path = "data/" + label+'/' + img.filename
+        img.save(img_path)
+        image_paths.append(img_path)
+
+    X, Y, num_classes = load_data()
+    print(X.shape, Y.shape, num_classes)
+    model = create_model_new_class(num_classes)
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
+    model.summary()
+    train(model, X, Y)
+    return "Success"
 
 if __name__ =='__main__':
-	app.run(debug = False, port = 8888) #create http server which people can access
+	app.run(debug=False, port=8000)
